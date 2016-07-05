eab313247ffbe5124fc4116994e14592ea0d4076
liuyaoif <liuyaoif@hotmail.com>
2016-07-05 22:39:37 +0800

    delete protobufDemo

--- Tools/ProtobufDemo/ProtocolDll/Step1-ProtoGen.bat
+++
@@ -1,8 +0,0 @@
-@echo Step1/3:把*.proto文件转换成*.cs文件
-
-set protoFiles=-i:loginServer.proto
-set CSharpFile=ProtocolCSharp.cs
-
-del %CSharpFile%
-ProtoGen\protogen %protoFiles% -o:%CSharpFile% -ns:com.pwrd.wuxiaprg
-pause
